231dea8e2bd56168f0655526a8f5464a514bc41a
Sajjadalgburi <dfgsajjad@gmail.com>
2023-11-25 13:29:07 -0700

    Math functionality and methods

---
+++ Math/script.js
@@ -0,0 +1,31 @@
+// Global variable
+let x = 3.14;
+
+// this method will round the number
+function round() {
+    x = Math.round(x);
+    console.log(x);
+}
+
+round(x)
+console.log("================================");
+
+// this method will round the number DOWN always
+function floor() {
+    x = Math.floor(x);
+    console.log(x);
+}
+
+floor(x)
+console.log("================================");
+// this method will round the number UP always
+
+function ceil() {
+    x = Math.round(x);
+    console.log(x);
+}
+
+ceil(x)
+
+// I give up
+
