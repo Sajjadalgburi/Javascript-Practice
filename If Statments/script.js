5c5421b00b91dfc3428d9bfcadff833edff0d6dc
Sajjadalgburi <dfgsajjad@gmail.com>
2023-11-26 14:46:00 -0700

    If Statments

---
+++ If Statments/script.js
@@ -0,0 +1,12 @@
+// If statment = a basic from of decison making
+// if a conditiopn is true, then do something
+// if not, do something else
+
+var myAge = 18;
+var buyHouse = 25;
+
+if (myAge >= buyHouse) {
+  console.log("You can buy a house!");
+} else {
+  console.log("You can't buy a house!");
+}
